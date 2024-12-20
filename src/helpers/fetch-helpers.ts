4dbf8a79910d956b7e1be34b0b39587eec057a61
Preisler25 <preisler.andras@gmail.com>
2024-12-20 16:33:07 +0100

    fixing: property error

--- src/helpers/fetch-helpers.ts
+++ src/helpers/fetch-helpers.ts
@@ -5,15 +5,13 @@ import { IAnimalsBySpecies } from "../interfaces/species.ts";
 
 export class FetchHelper {
   static async fetchAnimals(): Promise<IAnimal[]> {
-    const response = await axios.get<{ animals: IAnimal[] }>(
+    const response = await axios.get<IAnimal[]>(
       `${ENDPOINTS.ANIMALS}`
     );
     return response.data;
   }
 
-  static async fetchAnimalsBySpecies(): Promise<{
-    species: IAnimalsBySpecies[];
-  }> {
+  static async fetchAnimalsBySpecies(): Promise<IAnimalsBySpecies[]> {
     const response = await axios.get<IAnimalsBySpecies[]>(
       `${ENDPOINTS.ANIMALS_BY_SPECIES}`
     );
